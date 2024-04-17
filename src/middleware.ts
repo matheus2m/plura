a31ef79701639c43ab6ce81bbf3f0cf2ac51cef8
Matheus Moraes <matheus@xcoding.com.br>
2024-04-17 02:13:14 -0300

    first commit

---
+++ src/middleware.ts
@@ -0,0 +1,7 @@
+import { authMiddleware } from "@clerk/nextjs";
+export default authMiddleware({
+  publicRoutes: ["/site", "/api/uploadthing"]
+});
+export const config = {
+  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
