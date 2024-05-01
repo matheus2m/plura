d1cb25a801f2f47cb48ab832dfdc3292b4cc5663
Matheus Moraes <matheus@xcoding.com.br>
2024-05-01 20:11:20 -0300

    primsa config

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,16 +1,10 @@
-import Navigation from '@/components/site/navigation';
 import { ClerkProvider } from '@clerk/nextjs';
 import { dark } from '@clerk/themes';
 import React from 'react';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <ClerkProvider appearance={{ baseTheme: dark }}>
-      <main className='h-full'>
-        <Navigation />
-        {children}
-      </main>
-    </ClerkProvider>
+    <ClerkProvider appearance={{ baseTheme: dark }}>{children}</ClerkProvider>
   );
 };
 
